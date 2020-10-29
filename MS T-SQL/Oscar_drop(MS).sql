fd91f5892eb880ba513695a2264bcd725beafae7
vassylkorzh <61971564+vassylkorzh@users.noreply.github.com>
2020-10-29 08:54:43 +0100

    Addded MS folder

---
+++ MS T-SQL/Oscar_drop(MS).sql
@@ -0,0 +1,39 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2020-01-30 08:03:15.346
+
+-- foreign keys
+ALTER TABLE NadJakimFilmemPracolaw DROP CONSTRAINT NadJakimFilmem_Czlowiek;
+
+ALTER TABLE NadJakimFilmemPracolaw DROP CONSTRAINT NadJakimFilmem_Film;
+
+ALTER TABLE NadJakimFilmemPracolaw DROP CONSTRAINT NadJakimFilmem_Role;
+
+ALTER TABLE Nagroda DROP CONSTRAINT Nagroda_Ceremonia;
+
+ALTER TABLE ZwyciezcaFilm DROP CONSTRAINT Table_17_Film;
+
+ALTER TABLE ZwyciezcaCzlowiek DROP CONSTRAINT ZwyciezcaClowiek_Czlowiek;
+
+ALTER TABLE ZwyciezcaCzlowiek DROP CONSTRAINT ZwyciezcaClowiek_Nagroda;
+
+ALTER TABLE ZwyciezcaFilm DROP CONSTRAINT ZwyciezcaFilm_Nagroda;
+
+-- tables
+DROP TABLE Ceremonia;
+
+DROP TABLE Czlowiek;
+
+DROP TABLE Film;
+
+DROP TABLE NadJakimFilmemPracolaw;
+
+DROP TABLE Nagroda;
+
+DROP TABLE Role;
+
+DROP TABLE ZwyciezcaCzlowiek;
+
+DROP TABLE ZwyciezcaFilm;
+
+-- End of file.
+
